95b908d70d8ef8cb258d2ec17e9ae6b0f037d9f5
Yuri Serrano <yurijserrano@gmail.com>
2016-01-13 23:06:15 -0200

    Primeiro Programa

---
+++ programa1.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+"""
+Primeiro Programa em Python
+"""
+
+numero = "22"
+print("Eu acho que o número é:")
+palpite = raw_input(">")
+if palpite == numero:
+	print("Sim! Você Acertou o número!!!!")
+else:
+	print("Não - Você errou")
+raw_input("Aperte enter para continuar")
