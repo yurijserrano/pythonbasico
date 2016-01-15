489ef59c6a68a166508eefdd0615b984b9849134
Yuri Serrano <yurijserrano@gmail.com>
2016-01-15 12:33:22 -0200

    Sexto Programa

---
+++ programa6.py
@@ -0,0 +1,19 @@
+#-*- coding: UTF-8 -*-
+
+"""
+
+Utilização de variáveis
+
+Como utilizar uma variável
+
+"""
+
+
+variavel = 42
+x = 123.2
+abc_123 = "Uma string !!!!"
+
+
+print variavel
+print x
+print abc_123
