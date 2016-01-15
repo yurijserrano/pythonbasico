796b40594d4288801064b93eec1ea55503187ecb
Yuri Serrano <yurijserrano@gmail.com>
2016-01-15 12:26:28 -0200

    Quinto Programa

---
+++ programa5.py
@@ -0,0 +1,14 @@
+#-*- coding: UTF-8 -*-
+
+"""
+	Utilização do print
+
+	Com a vírgula conseguimos imprimir vários valores 
+	em um mesmo print
+"""
+
+
+
+print "Olá" , "Mundo", "!!!!!!"
+print "A resposta para a vida", "é que o universo e qualquer coisa é", 42
+print 3.141592, "é o pi", "!!!!!!"
